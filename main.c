4d837de26c508ee225fff54fac9cbb523b10b9d8
somyeong04 <144991811+somyeong04@users.noreply.github.com>
2023-11-09 16:03:42 +0900

    5

--- main.c
+++ main.c
@@ -5,9 +5,25 @@
 
 int main(int argc, char *argv[]) {
 	
-	char src[30] = "happy C programming";
+	FILE* fp;
+	char str[100];
+	int i;
+
+	//1. open file
+	fp = fopen("sample.txt","w");
+
+	//2. write file
+	for (i=0; i<3; i++){
+
+	//2-1. print "input a word"
+	printf("input a word:");
+	//2-2. scanf a string
+	scanf("%s", str);
+	//2-3. fprintf()
+	fprintf(fp, "%s\n", str);
+	}
 	
-	printf("문자열\"%s\"의 길이 : %i\n", src, strlen(src) );
-	return 0;
+	//3. close file
+	fclose(fp);
  
-}
+} 
