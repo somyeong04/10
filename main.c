e2ec53bc04217abd940ce6a0eebed9cba7de9f09
somyeong04 <144991811+somyeong04@users.noreply.github.com>
2023-11-09 16:10:53 +0900

    6

--- main.c
+++ main.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
-	FILE* fp;
-	char str[100];
-	int i;
-
-	//1. open file
-	fp = fopen("sample.txt","w");
-
-	//2. write file
-	for (i=0; i<3; i++){
-
-	//2-1. print "input a word"
-	printf("input a word:");
-	//2-2. scanf a string
-	scanf("%s", str);
-	//2-3. fprintf()
-	fprintf(fp, "%s\n", str);
-	}
-	
-	//3. close file
-	fclose(fp);
- 
-} 
+   
+   FILE* fp;
+   char c;
+   
+   fp = fopen("sample.txt","r");
+   if (fp == NULL)
+   {
+      printf("failed to open\n");
+      return 0;
+   }
+   
+   while( (c=fgetc(fp)) != EOF) //한글자 읽은게 마지막이 아니면
+   {
+     putchar (c); //한글자 출력 
+   }
+   
+   fclose(fp); 
+   
+   return 0;
+}
